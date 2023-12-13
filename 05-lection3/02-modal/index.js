40941993ab33db288b67dc9045fe99fb17cfbbcb
yudin-nikita <yudin.nikita.yudin@yandex.ru>
2023-12-13 22:53:55 +0300

    нужны будут правки

--- 05-lection3/02-modal/index.js
+++ 05-lection3/02-modal/index.js
@@ -1,19 +1,25 @@
-(function() {
-    const close = document.getElementById('modal-close');
-    const modal = document.getElementById('modal');
-    const modalOpen = document.getElementById('modal-open');
+// (function() {
+//     const close = document.getElementById('modal-close');
+//     const modal = document.getElementById('modal');
+//     const modalOpen = document.getElementById('modal-open');
 
-    if (!close || !modalOpen || !modal) {
-        return;
-    }
+//     if (!close || !modalOpen || !modal) {
+//         return;
+//     }
 
-    modalOpen.addEventListener('click', () => {
-        modal.style.display = 'block';
-        document.body.overflow = 'hidden';
-    });
+//     modalOpen.addEventListener('click', () => {
+//         modal.style.display = 'block';
+//         document.body.overflow = 'hidden';
+//     });
 
-    close.addEventListener('click', () => {
-        modal.style.display = 'none';
-        document.body.overflow = 'initial';
-    });
-})();
+//     close.addEventListener('click', () => {
+//         modal.style.display = 'none';
+//         document.body.overflow = 'initial';
+//     });
+// })();
+document.getElementById("modal-open").addEventListener("click",function() {
+    document.getElementById("modal").classList.add("open")
+})
+document.getElementById("modal-close").addEventListener("click",function() {
+    document.getElementById("modal").classList.remove("open")
+})
