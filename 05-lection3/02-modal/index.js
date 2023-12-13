8c1e0fa6f35cb3a052009ded117083be1a35967e
yudin-nikita <yudin.nikita.yudin@yandex.ru>
2023-12-13 23:36:37 +0300

    сделал модалку

--- 05-lection3/02-modal/index.js
+++ 05-lection3/02-modal/index.js
@@ -1,25 +1,14 @@
-// (function() {
-//     const close = document.getElementById('modal-close');
-//     const modal = document.getElementById('modal');
-//     const modalOpen = document.getElementById('modal-open');
+(function() {
+    const close = document.getElementById('modal-close');
+    const modal = document.getElementById('modal');
+    const modalOpen = document.getElementById('modal-open');
 
-//     if (!close || !modalOpen || !modal) {
-//         return;
-//     }
-
-//     modalOpen.addEventListener('click', () => {
-//         modal.style.display = 'block';
-//         document.body.overflow = 'hidden';
-//     });
-
-//     close.addEventListener('click', () => {
-//         modal.style.display = 'none';
-//         document.body.overflow = 'initial';
-//     });
-// })();
-document.getElementById("modal-open").addEventListener("click",function() {
-    document.getElementById("modal").classList.add("open")
-})
-document.getElementById("modal-close").addEventListener("click",function() {
-    document.getElementById("modal").classList.remove("open")
-})
+    modalOpen.addEventListener("click", () => {
+        modal.classList.add("open");
+        document.body.overflow = 'hidden';
+    })
+    close.addEventListener("click", () => {
+        modal.classList.remove("open");
+        document.body.overflow = 'initial';
+    })
+})();
